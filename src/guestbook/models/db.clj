cfbacb9c023fc2439d99dd8916f15b61a98cca4d
Paul Wittmann <paul@railslove.com>
2014-11-02 23:59:58 +0100

    db.clj: added create-guestbook-table (pp. 16-17).

---
+++ src/guestbook/models/db.clj
@@ -0,0 +1,19 @@
+(ns guestbook.models.db
+  (:require [clojure.java.jdbc :as sql])
+  (:import java.sql.DriverManager))
+
+(def db {:classname "org.sqlite.JDBC",
+         :subprotocol "sqlite",
+         :subname "db.sq3"
+         })
+
+(defn create-guestbook-table []
+  (sql/with-connection
+    db
+    (sql/create-table
+      :guestbook
+      [:id "INTEGER PRIMARY KEY AUTOINCREMENT"]
+      [:timestamp "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"]
+      [:name "TEXT"]
+      [:message "TEXT"])
+    (sql/do-commands "CREATE INDEX timestamp_index ON guestbook (timestamp)")))
