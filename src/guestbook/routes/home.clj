814543130140beb686c6942303a57eaf7f17ad2a
Paul Wittmann <paul@railslove.com>
2014-11-02 23:18:43 +0100

    Show some dummy comments, handle erroneous inputs.

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -1,17 +1,44 @@
 (ns guestbook.routes.home
   (:require [compojure.core :refer :all]
-            [guestbook.views.layout :as layout]))
+            [guestbook.views.layout :as layout]
+            [hiccup.form :refer :all]))
 
-(defn home []
+(defn show-guests []
+  [:ul.guests
+   (for [{:keys [message name timestamp]}
+         [{:message "Howdy" :name "Bob" :timestamp nil}
+          {:message "Hello" :name "Bob" :timestamp nil}]]
+     [:li
+       [:blockquote message]
+       [:p "-" [:cite name]]
+       [:time timestamp]])])
+
+(defn home [& [name message error]]
   (layout/common
     [:h1 "Hello World!"]
     [:p "Welcome to my guestbook"]
+    [:p error]
+    (show-guests)
     [:hr]
-    [:form
+    (form-to [:post "/"]
       [:p "Name:"]
-      [:input]
+      (text-field "name" name)
       [:p "Message:"]
-      [:textarea {:rows 10 :cols 40}]]))
+      (text-area {:rows 10 :cols 40} "message" message)
+      [:br]
+      (submit-button "comment"))))
+
+(defn save-message [name message]
+  (cond
+   (empty? name)
+     (home name message "Some dummy forgot to leave a name")
+   (empty? message)
+     (home name message "Don't you have something to say?")
+   :else
+     (do
+       (println name message)
+       (home))))
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (GET "/" [] (home))
+  (POST "/" [name message] (save-message name message)))
