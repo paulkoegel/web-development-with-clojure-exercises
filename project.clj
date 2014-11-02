88540c1ad2fec8aab8f4733e833a1d993d538b87
Paul Wittmann <paul@railslove.com>
2014-11-02 23:46:50 +0100

    Project.clj: added SQLite and JDBC dependencies (p. 15).

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
-                 [ring-server "0.3.1"]]
+                 [ring-server "0.3.1"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler guestbook.handler/app
          :init guestbook.handler/init
